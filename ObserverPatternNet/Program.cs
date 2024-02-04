f40f06b7a83f8e1cc3f5af76dcd2773304f3cbdc
vk1408 <84286885+vk1408@users.noreply.github.com>
2024-02-04 22:30:36 +0100

    Add project files.

---
+++ ObserverPatternNet/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace ObserverPatternNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var alarm = new Alarm();
+            alarm.Subscribe(new FireStation());
+            alarm.Notify();
+            alarm.Notify();
+            alarm.Notify();
+            alarm.Notify();
+            alarm.Notify();
+
+        }
+
+    }
+
+    public class Alarm : IObservable<int>, IDisposable
+    {
+        List<IObserver<int>> watchers = new();
+        public IDisposable Subscribe(IObserver<int> observer) 
+        { 
+            watchers.Add(observer);
+            return this;
+        }
+        int i = 0;
+        public void Dispose() => throw new NotImplementedException();
+        
+        public void Notify()
+        {
+            if (i>3)
+            {
+                watchers.ForEach(x=>x.OnCompleted());
+                return;
+            }
+            watchers.ForEach(x => x.OnNext(i++));
+        }
+    }
+    public class FireStation : IObserver<int>
+    {
+        public void Alert(Alarm value)
+        {
+            Console.WriteLine($"{nameof(FireStation)}: RESPONDING!");
+        }
+
+        // Provider has finished sending data
+        public void OnCompleted()
+        {
+            Console.WriteLine($"{nameof(FireStation)}: complete");
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine($"{nameof(FireStation)}: error");
+        }
+
+        // value in Provider changed 
+        public void OnNext(int value)
+        {
+            Console.WriteLine($"{nameof(FireStation)}: next: {value}");
+        }
+    }
+}
---
+++ ObserverPatternRaw/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace ObserverPatternRaw
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var alarm = new Alarm("person injured");
+            alarm.AddWatcher(new FireStation());
+            alarm.AddWatcher(new PoliceStation());
+            alarm.AddWatcher(new HospitalStation());
+            alarm.Notify();
+            alarm.Message = "house burning";
+            alarm.Notify();
+
+        }
+    }
+    public class Alarm
+    {
+        public string Message { get; set; }
+        List<IWatcher<Alarm>> watchers = new();
+        public Alarm(string message)
+        {
+            Message = message;
+        }
+        public void AddWatcher(IWatcher<Alarm> alarmWatcher)
+        {
+            watchers.Add(alarmWatcher);
+        }
+        public void Notify()
+        {
+            foreach (var w in watchers) 
+            {
+                w.Alert(this);
+            }
+        }
+    }
+    public interface IWatcher<T>
+    {
+        void Alert(T value);
+    }
+    public class FireStation : IWatcher<Alarm>
+    {
+        public void Alert(Alarm value)
+        {
+            Console.WriteLine($"{nameof(FireStation)} responding to {value.Message}");
+        }
+    }
+    public class HospitalStation : IWatcher<Alarm>
+    {
+        public void Alert(Alarm value)
+        {
+            Console.WriteLine($"{nameof(HospitalStation)} responding to {value.Message}");
+        }
+    }
+
+    public class PoliceStation : IWatcher<Alarm>
+    {
+        public void Alert(Alarm value)
+        {
+            Console.WriteLine($"{nameof(PoliceStation)} responding to {value.Message}");
+        }
+    }
+
+ 
+}
